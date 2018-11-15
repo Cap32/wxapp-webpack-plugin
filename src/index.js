60603e6f834750d3063ee9676b04facf7ee32365
3went2 <webb.png@gmail.com>
2018-11-15 18:47:21 +0800

    add and expose `createTarget` function

--- src/index.js
+++ src/index.js
@@ -30,23 +30,28 @@ const stripExt = path => {
 	return join(dir, name);
 };
 
-const miniProgramTarget = compiler => {
-	const { options } = compiler;
-	compiler.apply(
-		new JsonpTemplatePlugin(options.output),
-		new FunctionModulePlugin(options.output),
-		new NodeSourcePlugin(options.node),
-		new LoaderTargetPlugin('web')
+export const createTarget = function createTarget(name) {
+	const miniProgramTarget = compiler => {
+		const { options } = compiler;
+		compiler.apply(
+			new JsonpTemplatePlugin(options.output),
+			new FunctionModulePlugin(options.output),
+			new NodeSourcePlugin(options.node),
+			new LoaderTargetPlugin('web')
+		);
+	};
+
+	// eslint-disable-next-line no-new-func
+	const creater = new Function(
+		`var t = arguments[0]; return function ${name}(c) { return t(c); }`
 	);
+	return creater(miniProgramTarget);
 };
 
 export const Targets = {
-	Wechat(compiler) {
-		return miniProgramTarget(compiler);
-	},
-	Alipay(compiler) {
-		return miniProgramTarget(compiler);
-	}
+	Wechat: createTarget('Wechat'),
+	Alipay: createTarget('Alipay'),
+	Baidu: createTarget('Baidu')
 };
 
 export default class WXAppPlugin {
